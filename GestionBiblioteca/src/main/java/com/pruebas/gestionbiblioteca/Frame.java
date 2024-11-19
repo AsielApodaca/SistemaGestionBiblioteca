12ae57b5595ee5a1e64e52ed25e1071a55c849ef
luisa <moraLF20@outlook.com>
2024-11-19 15:17:34 -0700

    proyecto de GUI
    falta terminar unos detalles

---
+++ GestionBiblioteca/src/main/java/com/pruebas/gestionbiblioteca/Frame.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.pruebas.gestionbiblioteca;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author luisa M
+ */
+public class Frame extends JFrame {
+    
+}
---
+++ GestionBiblioteca/src/main/java/com/pruebas/gestionbiblioteca/GestionBiblioteca.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.pruebas.gestionbiblioteca;
+
+/**
+ *
+ * @author luisa M
+ */
+public class GestionBiblioteca {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
